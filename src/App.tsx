b00ca6dfa4801797a49fad91c499db82b8fbc29d
Nadhif Fathur Rahman <nadhiffathur@gmail.com>
2026-03-10 23:30:17 +0700

    Refactor routing components in App.tsx for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import HomePage from "./pages/HomePage";
-import ContentPage from "./pages/ContentPage";
-import AboutPage from "./pages/AboutPage";
-import ContactPage from "./pages/ContactPage";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import ContentPage from "./pages/Content";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 export default function App() {
   return (
@@ -12,7 +13,7 @@ export default function App() {
       <div className="flex-1">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/content" element={<Content />} />
+          <Route path="/content" element={<ContentPage />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
